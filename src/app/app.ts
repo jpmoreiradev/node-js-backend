538361794944b1f61383917cb1a17a38c3a660f3
JPMoreira <jp02120123@gmail.com>
2022-08-27 16:20:10 -0300

    serve configured

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import  logger   from "../config/logger";
+
+const app = express();
+
+
+
+app.get('/', (req, res ) => {
+ res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  logger.info('Serve is running');
+})
+
+
+ 
